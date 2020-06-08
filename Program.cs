d70914f7a31b4592b723cc390b1104445beb2f4c
Matan Swisa <matanswisa01@gmail.com>
2020-06-08 19:12:40 +0300

    added comments

--- Program.cs
+++ Program.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.IO;
 
 namespace MultiServer
 {
     class Program
     {
-        private static readonly Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        private static readonly List<Socket> clientSockets = new List<Socket>();
-        private const int BUFFER_SIZE = 2048;
-        private const int PORT = 8080;
+        private static readonly Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); // connection with protocol TCP
+        private static readonly List<Socket> clientSockets = new List<Socket>(); // clients list
+        private const int BUFFER_SIZE = 50000;
+        private const int PORT = 8080; 
         private static readonly byte[] buffer = new byte[BUFFER_SIZE];
         private static int online = 0;
 
@@ -24,13 +25,17 @@ namespace MultiServer
             CloseAllSockets();
         }
 
+        /// <summary>
+        /// Starting socket server
+        /// </summary>
         private static void SetupServer()
         {
             Console.WriteLine("Setting up server...");
             serverSocket.Bind(new IPEndPoint(IPAddress.Any, PORT));
-            serverSocket.Listen(0);
+            serverSocket.Listen(4);
             serverSocket.BeginAccept(AcceptCallback, null);
-            Console.WriteLine("Server setup complete");
+            Console.WriteLine("Server setup complete \n" +
+                "Ipv4 of the host is: " + GetLocalIPAddress());
         }
 
         /// <summary>
@@ -48,6 +53,11 @@ namespace MultiServer
 
             serverSocket.Close();
         }
+
+        /// <summary>
+        /// sending a string to all connecting clients
+        /// </summary>
+        /// <param name="s"></param>
         private static void sentToAll(string s)
         {
             foreach (Socket socket in clientSockets)
@@ -56,6 +66,11 @@ namespace MultiServer
                 socket.Send(data);
             }
         }
+
+        /// <summary>
+        /// accepting a connection of  socket.
+        /// </summary>
+        /// <param name="AR"></param>
         private static void AcceptCallback(IAsyncResult AR)
         {
             Socket socket;
@@ -68,15 +83,18 @@ namespace MultiServer
             {
                 return;
             }
-
             online++; // number of people connected
             clientSockets.Add(socket);
             socket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, socket);
             Console.WriteLine("Client number {0} connected, waiting for request...", online);
-            sentToAll("Client number " + online + " connected");
+         //   sentToAll(ONLY_MESSAGE+"Client number " + online + " connected ");
             serverSocket.BeginAccept(AcceptCallback, null);
         }
 
+        /// <summary>
+        /// Reciving data from the client
+        /// </summary>
+        /// <param name="AR"></param>
         private static void ReceiveCallback(IAsyncResult AR)
         {
             Socket current = (Socket)AR.AsyncState;
@@ -96,20 +114,19 @@ namespace MultiServer
             }
 
 
-
-            byte[] recBuf = new byte[received];
+            byte[] recBuf = new byte[received]; //recive data
             Array.Copy(buffer, recBuf, received);
-            string text = Encoding.ASCII.GetString(recBuf);
+            string text = Encoding.ASCII.GetString(recBuf); //casting the bytes to a string
             Console.WriteLine("Received Text: " + text);
 
-            if (text.ToLower() == "get time") // Client requested time
+            if (text.ToLower().Contains("get time")) // Client requested time
             {
                 Console.WriteLine("Text is a get time request");
                 byte[] data = Encoding.ASCII.GetBytes(DateTime.Now.ToLongTimeString());
-                current.Send(data);
+                sentToAll(data.ToString());
                 Console.WriteLine("Time sent to client is: " + DateTime.Now.ToLongTimeString());
             }
-            else if (text.ToLower() == "exit") // Client wants to exit gracefully
+            else if (text.ToLower()=="exit") // Client wants to exit gracefully
             {
                 // Always Shutdown before closing
                 current.Shutdown(SocketShutdown.Both);
@@ -120,11 +137,27 @@ namespace MultiServer
             }
             else
             {
-
                 sentToAll(text);
             }
 
             current.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, current);
+        }
+
+        /// <summary>
+        ///  returns the IPv4 of the computer host. 
+        /// </summary>
+        /// <returns></returns>
+        public static string GetLocalIPAddress() // this function returns the local IPv4 address of the host.
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
         }
     }
 }
